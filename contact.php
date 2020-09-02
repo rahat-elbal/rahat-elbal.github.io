ac2c53c8f0d24e0033c78f50d554a711bb0b3c1e
rahat-elbal <69808626+rahat-elbal@users.noreply.github.com>
2020-09-02 13:39:12 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="ar" dir="ltr"></html>
+  <head>
+    <title>rahat-elbal ﺭاﺣﺔاﻟﺒﺎﻝ</title>
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+   <div class="wrap">
+   <div class="container">
+    <div class="row">
+     <div class="col-md-6 d-flex align-items-center">
+      <p class="mb-0 phone pl-md-2">
+       <a href="#" class="mr-2"><span class="fa fa-phone mr-1"></span> 212662482469</a> 
+       <a href="#"><span class="fa fa-paper-plane mr-1"></span> bhyer09@gmail.com</a>
+      </p>
+     </div>
+     <div class="col-md-6 d-flex justify-content-md-end">
+      <div class="social-media">
+         <p class="mb-0 d-flex">
+          <a href="https://web.facebook.com/said.bhyer" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+          <a href="https://twitter.com/SaidBhyer" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+          <a href="https://www.instagram.com/bhyersaid/" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+        </p>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+    
+   <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+     <div class="container">
+       <a class="navbar-brand" href="index.html"></a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="oi oi-menu"></span> Menu
+       </button>
+
+       <div class="collapse navbar-collapse" id="ftco-nav">
+         <ul class="navbar-nav ml-auto">
+    <li class="nav-item active"><a href="contact.html" class="nav-link">اﺗﺼﻞ ﺑﻨﺎ</a></li>
+    <li class="nav-item"><a href="research.html" class="nav-link">ﺃﺑﺤﺎﺕ ﻭﺩﺭاﺳﺎﺕ</a></li>
+        <li class="nav-item"><a href="casestudy.html" class="nav-link">ﺣﺎﻻﺕ</a></li>
+        <li class="nav-item"><a href="services.html" class="nav-link">ﺧﺪﻣﺎﺕ</a></li>
+    <li class="nav-item"><a href="about.html" class="nav-link">ﻣﻦ ﻧﺤﻦ</a></li>
+    <li class="nav-item"><a href="index.html" class="nav-link">اﻟﺼﻔﺤﺔ اﻟﺮﺋﻴﺴﻴﺔ</a></li>
+         </ul>
+       </div>
+     </div>
+   </nav>
+    <!-- END nav -->
+    
+
+
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('imagescouns/contact.jpg');" data-stellar-background-ratio="0.5">
+      
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-end justify-content-center">
+          <div class="col-md-9 ftco-animate mb-5 text-center">
+        
+            <h1 class= "mb-0 bread name-darkblue">اﺗﺼﻞ ﺑﻨﺎ</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  <section class="ftco-section bg-light">
+   <div class="container">
+    <div class="row justify-content-center">
+     <div class="col-md-12">
+      <div class="wrapper px-md-4">
+       <div class="row mb-5">
+        <div class="col-md-4">
+         <div class="dbox w-100 text-center">
+              <div class="icon d-flex align-items-center justify-content-center">
+               <span class="fa fa-phone"></span>
+              </div>
+              <div class="text">
+                 <p><span>Phone:</span> <a href="tel://212662482469">212662482469</a></p>
+               </div>
+              </div>
+        </div>
+        <div class="col-md-4">
+         <div class="dbox w-100 text-center">
+              <div class="icon d-flex align-items-center justify-content-center">
+               <span class="fa fa-paper-plane"></span>
+              </div>
+              <div class="text">
+                 <p><span>Email:</span> <a href="mailto:bhyer09@gmail.com">bhyer09@gmail.com</a></p>
+               </div>
+              </div>
+        </div>
+        <div class="col-md-4">
+         <div class="dbox w-100 text-center">
+              <div class="icon d-flex align-items-center justify-content-center">
+               <span class="fa fa-globe"></span>
+              </div>
+              <div class="text">
+                 <p><span>Website</span> <a href="#">rahat-elbal.github.io</a></p>
+               </div>
+              </div>
+        </div>
+       </div>
+       <div class="row no-gutters">
+        <div class="col-md-7">
+         
+                </div>
+                <!--
+        <div class="col-md-5 order-md-first d-flex align-items-stretch">
+                  <div id="map" class="map"></div>
+                  -->
+        </div>
+       </div>
+      </div>
+     </div>
+    </div>
+   </div>
+  </section>
+
+    <footer class="ftco-footer">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2 logo"><a href="#"></a></h2>
+              <p></p>
+              <ul class="ftco-footer-social list-unstyled mt-2">
+                <li class="ftco-animate"><a href="https://twitter.com/SaidBhyer"><span class="fa fa-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="https://web.facebook.com/said.bhyer"><span class="fa fa-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.instagram.com/bhyersaid/"><span class="fa fa-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2"></h2>
+              <ul class="list-unstyled">
+                <!--
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span></a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span></a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span></a></li>
+                -->
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2"></h2>
+              <ul class="list-unstyled">
+            
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2"></h2>
+              <ul class="list-unstyled">
+                <!--
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span></a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span></a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span></a></li>
+                -->
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4">
+             <h2 class="ftco-heading-2"></h2>
+             <div class="block-23 mb-3">
+               <ul>
+                  <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">ﻟﺪﻳﻚ ﺃﺳﺌﻠﺔ</span></a></li>
+                  <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">212662482469</span></a></li>
+                 <li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">bhyer09@gmail.com</span></a></li>
+               </ul>
+             </div>
+            </div>
+          </div>
+        </div>
+   </div>
+   <section class="ftco-appointment ftco-section img" style="background-image: url(images/bg_2.jpg);">
+        <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 half ftco-animate">
+                <h2 class="mb-4">اﺑﻌﺚ ﺭﺳﺎﻟﺔ &amp; اﺗﺼﻞ ﺑﻨﺎ</h2>
+                <form action="php/contactus.php" method="post" id="contactForm" class="appointment">
+                    <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                      <input type="text" class="form-control" id="nom" name="nom" placeholder="اﻻﺳﻢ اﻟﻜﺎﻣﻞ">
+                    </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                      <input type="text" class="form-control" id="mail" name="mail" placeholder="Email">
+                    </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                <div class="form-field">
+                              <div class="select-wrap">
+                      <div class="icon"><span class="fa fa-chevron-down"></span></div>
+                      <select name="service" id="service" class="form-control">
+                          <option value="">خدمات</option>
+                        <option value="">مشاكل العلاقات مع الناس</option>
+                        <option value="">الاستشارات الزواجية</option>
+                        <option value="">علاج الاكتئاب</option>
+                        <option value="">المشاكل الأسرية</option>
+                        <option value="">المشاكل الشخصية</option>
+                        <option value="">مشاكل العمل</option>
+                      </select>
+                    </div>
+                      </div>
+                            </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                      <textarea name="commentaire" id="commentaire" cols="30" rows="7" class="form-control" placeholder="ﺭﺳﺎﻟﺔ"></textarea>
+                    </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                      <input type="submit" value="ﺇﺑﻌﺚ ﺭﺳﺎﻟﺔ" class="btn btn-primary py-3 px-4">
+                      
+                    </div>
+                            </div>
+                    </div>
+          </form>
+            </div>
+        </div>
+    </div>
+</section>
+      <div class="container-fluid px-0 py-5 bg-black">
+       <div class="container">
+        <div class="row">
+           <div class="col-md-12">
+  
+             <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved rahat-elbal| This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+           </div>
+         </div>
+       </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
